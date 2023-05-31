2716980dd9c4aa098d21c958454a759c90b24913
NovicovMykhailo <mikenovicov@gmail.com>
2023-05-31 17:02:57 +0300

    add some

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { useFonts } from "expo-font";
+
 import Registration from "./Screens/RegistrationScreen";
 
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-      "Roboto-Regular": require("./assets/fonts/Roboto-Regular.otf"),
-      "Roboto-Medium": require("./assets/fonts/Roboto-Medium.otf"),
-      "Roboto-Bold": require("./assets/fonts/Roboto-Bold.otf"),
-    });
-  
-   if (!fontsLoaded) {
-     return null;
-   }
-  
+
+
   return (
-    <View style={{ width: "100%"}}>
+    <View style={{ width: "100%"}} >
       <Registration />
     </View>
   );
--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -11,7 +11,8 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 import { useState } from "react";
-import image from "../assets/Photo_BG@2x.png";
+import image from "../assets/Photo_BG2x.png";
+import { useFonts } from "expo-font";
 
 const Registration = () => {
   const [login, setLogin] = useState("");
@@ -22,6 +23,14 @@ const Registration = () => {
     password: false,
     login: false,
   });
+
+  const [fontsLoaded] = useFonts({
+    Roboto: require("../assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   const handleInputFocus = textinput => {
     setIsFocused({
@@ -38,57 +47,55 @@ const Registration = () => {
   };
 
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback style={styles.base}>
       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <View style={styles.container}>
+        <View style={styles.box}>
           <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={styles.view}>
-            <View style={styles.container}>
-              <View style={styles.userPhoto}>
-                <TouchableOpacity style={styles.takePhotoOut}>
-                  <Text style={styles.insideText}>+</Text>
-                </TouchableOpacity>
-              </View>
-
-              <Text style={styles.title}>Реєстрація</Text>
-              <TextInput
-                onFocus={() => handleInputFocus("login")}
-                onBlur={() => handleInputBlur("login")}
-                style={isFocused.login ? styles.inputOnFocus : styles.input}
-                placeholder="Логін"
-                onChangeText={setLogin}
-                value={login}
-                inputMode="text"
-                placeholderTextColor="#BDBDBD"
-              />
-              <TextInput
-                onFocus={() => handleInputFocus("email")}
-                onBlur={() => handleInputBlur("email")}
-                placeholder="Адреса електронної пошти"
-                style={isFocused.email ? styles.inputOnFocus : styles.input}
-                onChangeText={setEmail}
-                value={email}
-                inputMode="email"
-                placeholderTextColor="#BDBDBD"
-
-              />
-              <TextInput
-                onFocus={() => handleInputFocus("password")}
-                onBlur={() => handleInputBlur("password")}
-                placeholder="Пароль"
-                style={isFocused.password ? styles.inputOnFocus : styles.input}
-                onChangeText={setPassword}
-                value={password}
-                textContentType="password"
-                placeholderTextColor="#BDBDBD"
-                secureTextEntry="true"
-              />
-              <TouchableOpacity style={styles.btn} onClick={onLogin}>
-                <Text style={styles.btnText}>Зареєстуватися</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.bottomTextContainer}>
-                <Text style={styles.bottomText}>Вже є акаунт? Увійти</Text>
+            <View style={styles.userPhoto}>
+              <TouchableOpacity style={styles.takePhotoOut}>
+                <Text style={styles.insideText}>+</Text>
               </TouchableOpacity>
             </View>
+
+            <Text style={styles.title}>Реєстрація</Text>
+            <TextInput
+              onFocus={() => handleInputFocus("login")}
+              onBlur={() => handleInputBlur("login")}
+              style={isFocused.login ? styles.inputOnFocus : styles.input}
+              placeholder="Логін"
+              onChangeText={setLogin}
+              value={login}
+              inputMode="text"
+              placeholderTextColor="#BDBDBD"
+            />
+            <TextInput
+              onFocus={() => handleInputFocus("email")}
+              onBlur={() => handleInputBlur("email")}
+              placeholder="Адреса електронної пошти"
+              style={isFocused.email ? styles.inputOnFocus : styles.input}
+              onChangeText={setEmail}
+              value={email}
+              inputMode="email"
+              placeholderTextColor="#BDBDBD"
+            />
+            <TextInput
+              onFocus={() => handleInputFocus("password")}
+              onBlur={() => handleInputBlur("password")}
+              placeholder="Пароль"
+              style={isFocused.password ? styles.inputOnFocus : styles.input}
+              onChangeText={setPassword}
+              value={password}
+              textContentType="password"
+              placeholderTextColor="#BDBDBD"
+              secureTextEntry={true}
+            />
+
+            <TouchableOpacity style={styles.btn} onClick={onLogin}>
+              <Text style={styles.btnText}>Зареєстуватися</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.bottomTextContainer}>
+              <Text style={styles.bottomText}>Вже є акаунт? Увійти</Text>
+            </TouchableOpacity>
           </KeyboardAvoidingView>
         </View>
       </ImageBackground>
@@ -98,50 +105,55 @@ const Registration = () => {
 export default Registration;
 
 const styles = StyleSheet.create({
+  base: { },
+  box: {
 
-  container: {
     height: "100%",
-    flex: 1,
-    flexDirection: "column",
-    fontFamily: "Roboto-Regular",
-    position: "relative",
-    textAlign: "center",
     justifyContent: "flex-end",
+    textAlign: "center",
+    position: "relative",
+    flexDirection: "column",
+
   },
   view: {
+
+
     backgroundColor: "#ffffff",
     borderColor: "#ffffff",
     borderWidth: 5,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    paddingTop: "92px",
-    paddingLeft: "16px",
-    paddingRight: "16px",
-    paddingBottom: "43px",
+    paddingTop: 92,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 43,
   },
   userPhoto: {
-    width: "120px",
-    height: "120px",
-    borderRadius: "16px",
+    width: 120,
+    height: 120,
+    borderRadius: 16,
     backgroundColor: "#F6F6F6",
     position: "absolute",
-    top: -152,
+    top: -65,
     left: "48%",
-    transform: [{ translateX: -50, translateY: -50 }],
+    transform: [{ translateX: -50 }],
   },
   takePhotoOut: {
     position: "absolute",
-    width: "25px",
-    height: "25px",
-    right: "-11px",
-    borderRadius: "50%",
+    width: 25,
+    height: 25,
+    right: -11,
+    borderRadius: 50,
     backgroundColor: "#FFFFFF",
-    borderWidth: "1px",
+    borderWidth: 1,
     borderStyle: "solid",
     borderColor: "#FF6C00",
-    top: "81px",
+    top: 81,
     display: "flex",
     alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Roboto",
+    fontWeight: "100",
   },
   input: {
     height: 50,
@@ -149,74 +161,73 @@ const styles = StyleSheet.create({
     padding: 16,
     fontSize: 16,
     backgroundColor: "#F6F6F6",
-    borderWidth: "1px",
+    borderWidth: 1,
     borderColor: "#E8E8E8",
     borderStyle: "solid",
-    borderRadius: "8px",
+    borderRadius: 8,
   },
   inputOnFocus: {
     height: 50,
     marginTop: 16,
     padding: 16,
     backgroundColor: "#FFFFFF",
-    borderWidth: "1px",
+    borderWidth: 1,
     borderColor: "#FF6C00",
-    outlineStyle: "none",
+
     borderStyle: "solid",
-    borderRadius: "8px",
+    borderRadius: 8,
     fontWeight: "400",
     fontSize: 16,
     color: "#212121",
   },
   title: {
     fontStyle: "normal",
-    fontWeight: 500,
+    fontWeight: "500",
     fontSize: 30,
     lineHeight: 35,
     textAlign: "center",
     letterSpacing: 0.01,
     color: "#212121",
-    marginBottom: "16px",
+    marginBottom: 16,
+    fontFamily: "Roboto",
   },
   image: {
-    flex: 1,
+
     resizeMode: "cover",
+    height: '100%',
   },
   btn: {
     backgroundColor: "#FF6C00",
-    borderRadius: "100px",
-    paddingTop: "16px",
-    paddingBottom: "16px",
-    paddingLeft: "32px",
-    paddingRight: "32px",
-    marginTop: "43px",
+    borderRadius: 100,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 32,
+    paddingRight: 32,
+    marginTop: 43,
     display: "flex",
     alignItems: "center",
   },
   btnText: {
     fontWeight: "400",
-    fontSize: "16px",
-    lineHeight: "19px",
+    fontSize: 16,
+    lineHeight: 19,
     color: "#FFFFFF",
   },
-
   insideText: {
     fontSize: 13,
-    fontWeight: 100,
+    fontWeight: "100",
     transform: [{ scale: 2 }],
     color: "#FF6C00",
+    fontFamily: "Roboto",
   },
-  bottomTextContainer: {},
-
   bottomText: {
-    padding: 16,
+    paddingTop: 16,
     color: "#1B4371",
     fontStyle: "normal",
-    fontWeight: 400,
-    fontSize: "16px",
-    lineHeight: "19px",
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 19,
     textAlign: "center",
+    fontFamily: "Roboto",
   },
 });
-
-// https://github.com/react-native-image-picker/react-native-image-picker
