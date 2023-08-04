1e09f9b14defaf6462796346539b3fc23c9cf3fb
NovicovMykhailo <mikenovicov@gmail.com>
2023-08-04 18:27:15 +0300

    Update thunks.js

--- src/redux/posts/thunks.js
+++ src/redux/posts/thunks.js
@@ -1,6 +1,9 @@
 // import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+// import { storage } from "../../config"; //firebase
+// import { ref } from "firebase/storage"; //firebase
+
 
 
 /* WORK WITH DB
@@ -29,6 +32,22 @@ const loginDB = async ({ email, password }) => {
 //   id: res.user.uid,
 //   user_photo: storageRef._location.path,
 // });
+
+
+///add photo to db and get storage
+
+import { getStorage, ref } from "firebase/storage";
+
+// Create a reference with an initial file path and name
+const storage = getStorage();
+const pathReference = ref(storage, 'images/stars.jpg');
+
+// Create a reference from a Google Cloud Storage URI
+const gsReference = ref(storage, 'gs://bucket/images/stars.jpg');
+
+// Create a reference from an HTTPS URL
+// Note that in the URL, characters are URL escaped!
+const httpsReference = ref(storage, 'https://firebasestorage.googleapis.com/b/bucket/o/images%20stars.jpg');  
 
  */
 
